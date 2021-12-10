7f30d58b5d1be49e5c40e773ebd8b4cf75d65a2b
Turyan Azizov <turyanazizov13@gmail.com>
2021-12-10 21:22:48 +0400

    Portfolioma AutoWrite text eleva etdim

--- ProjectFrontEnd/static/js/particulars.js
+++ ProjectFrontEnd/static/js/particulars.js
@@ -109,11 +109,8 @@ particlesJS("particles-js", {
     "retina_detect": true
 });
 var count_particles, stats, update;
-document.body.appendChild(stats.domElement);
 count_particles = document.querySelector('.js-count-particles');
 update = function () {
-    stats.begin();
-    stats.end();
     if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) {
         count_particles.innerText = window.pJSDom[0].pJS.particles.array.length;
     }
